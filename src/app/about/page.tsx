8b72409c20cccf5d8783a33cef90594a0071d97a
Ashish Vishwakarma <35719628+d2867ash@users.noreply.github.com>
2024-08-15 16:41:19 +0530

    My First Git Hub
    My First Git Hub

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function RegisterPage() {
+    return (
+      <><div>
+        Welcome to new page
+      </div>
+      </>
+    );
+  }
---
+++ src/app/components/BootstrapClient.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { Navbar, NavDropdown, Nav } from 'react-bootstrap'
+
+export const NavBarBrand = Navbar.Brand;
+export const NavbarToggle = Navbar.Toggle;
+export const NavbarCollapse = Navbar.Collapse;
+export const NavLink = Nav.Link;
+export const NavDropdownDivider = NavDropdown.Divider;
+export const NavDropdownItem = NavDropdown.Item;
+
+
+export * from 'react-bootstrap'

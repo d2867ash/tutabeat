66e0b590cd3c7654f5d641a865d7510b37f1088a
Ashish Vishwakarma <35719628+d2867ash@users.noreply.github.com>
2024-08-15 18:06:07 +0530

    Fix the UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+       
+       <link
+         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+         rel="stylesheet"
+         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+         crossOrigin="anonymous"
+       />
+
+     </head>
       <body className={inter.className}>
         {children}
         
